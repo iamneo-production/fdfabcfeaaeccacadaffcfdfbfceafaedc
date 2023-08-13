dd1aac7da1c53564d934642dad37457416ffbaa3
Iamneo <techadmin@iamneo.ai>
2023-08-13 15:20:30 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/springapp/SpringappApplication.java
+++ springapp/springapp/src/main/java/com/examly/springapp/springapp/SpringappApplication.java
@@ -1,4 +1,4 @@
-package com.examly.springapp.springapp;
+package com.examly.springapp.;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springapp/springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
+++ springapp/springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.examly.springapp.springapp;
+package com.examly.springapp;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
