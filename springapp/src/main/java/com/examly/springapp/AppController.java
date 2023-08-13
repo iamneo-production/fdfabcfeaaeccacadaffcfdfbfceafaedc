6f9eea0709a8324d8673df2f6570987f8cb14ff3
Iamneo <techadmin@iamneo.ai>
2023-08-13 15:42:25 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/AppController.java
@@ -0,0 +1,35 @@
+package com.examly.springapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;  
+import java.util.*;
+@RestController
+public class AppController {
+    @Autowired
+    Dao dao;
+    @PostMapping("/saveTask")
+    public String saveTask(@RequestBody Model detail) {
+        dao.save(detail);
+        return "Sucess";
+    }
+    @GetMapping("/allTasks")
+    List<Model> allTasks()
+    {
+        List<Model> all=(List<Model>) dao.findAll();
+        return all;
+    }
+    @GetMapping("/deleteTask")
+    public void deleteTask(@RequestParam int  id)
+    {
+        dao.deleteById(id);
+    }
+    @GetMapping("/getTask")
+    public Model getTask(@RequestParam int id){
+        Model a =dao.findById(id).orElse(null);
+        return a;
+    }
+}
---
+++ springapp/src/main/java/com/examly/springapp/Model.java
@@ -0,0 +1,53 @@
+package com.examly.springapp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table
+public class Model {
+    @Id
+    @Column  
+    private int taskId;
+    @Column
+    private String taskHolderName;
+    @Column
+    private String taskDate;
+    @Column
+    private String taskName;
+    @Column
+    private String taskStatus;
+    public int getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+    public String getTaskDate() {
+        return taskDate;
+    }
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+    
+}
